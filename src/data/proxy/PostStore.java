06b50fc05261ae15799d3e6889561d57e4f0dfe5
Nathan Coleman <nathandanielcoleman@gmail.com>
2015-09-14 15:30:34 -0400

    Clean up

--- src/data/proxy/PostStore.java
+++ src/data/proxy/PostStore.java
@@ -23,13 +23,13 @@ public class PostStore {
     /**
      * Writes a post to storage.
      * 
-     * @param post
+     * @param post The Post object to store
      * @throws IllegalArgumentException
      *             if post is null
      */
     public void write(Post post) {
         if (post == null) {
-            throw new IllegalArgumentException("Post canot be null.");
+            throw new IllegalArgumentException("Post cannot be null.");
         }
         String user = post.getUser();
         if (!postsByUser.containsKey(user)) {
@@ -41,17 +41,17 @@ public class PostStore {
     /**
      * Gets the posts for the specified user from storage.
      * 
-     * @param user
+     * @param id The String id for the UserProfile to retrieve
      * @return the user's posts
      * @throws IllegalArgumentException
      *             if user is null
      */
-    public List<Post> getPostsByUser(String user) {
-        if (user == null) {
+    public List<Post> getPostsByUser(String id) {
+        if (id == null) {
             throw new IllegalArgumentException("User cannot be null.");
         }
-        if (postsByUser.containsKey(user)) {
-            return postsByUser.get(user);
+        if (postsByUser.containsKey(id)) {
+            return postsByUser.get(id);
         } else {
             return new ArrayList<Post>();
         }
